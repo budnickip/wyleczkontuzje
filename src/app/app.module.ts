04585db59a54ce06024f180ad49a745fc94b46ba
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2019-09-24 11:29:13 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { AppRoutingModule } from './app-routing.module';
+import { KneeComponent } from './knee/knee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    FooterComponent,
+    MenuComponent,
+    KneeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/knee/knee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-knee',
+  templateUrl: './knee.component.html',
+  styleUrls: ['./knee.component.css']
+})
+export class KneeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
